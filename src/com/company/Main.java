de8589ecd72322925fa7a653ea51e088c005080f
cmlimm <cmothlimm@gmail.com>
2016-11-21 18:59:23 +0300

    подсчет очков решили проблему с амбивалентностью туза прототип стола в мэйне

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         List<Player> players = new LinkedList<>();
-        players.add(new Computer());
-        players.add(new Computer());
-        players.add(new Human());
+        players.add(new Computer(new LimitIntellect(14)));
+        players.add(new Computer(new LimitIntellect(20)));
+        players.add(new Human(new ConsoleIntellect()));
         Dealer dealer = new Dealer();
         players.add(dealer);
 
@@ -21,6 +21,7 @@ public class Main {
 
         for (Player player : players) {
             while(true) {
+                System.out.println(player.hand.getScore() + ": " + player.hand);
                 Command command = player.decision();
                 if (command == Command.STAND)
                     break;
